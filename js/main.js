1b0dd458d007701152fdc1d90d1cf2c7b4aae70d
KennyAle <kennyviver@gmail.com>
2023-09-27 13:38:09 -0700

    Image Optimization

--- js/main.js
+++ js/main.js
@@ -18,3 +18,32 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden') 
 hiddenElements.forEach((el) => observer.observe(el))
+
+const testimonies = document.querySelectorAll('.testimony');
+let currentIndex = 0;
+
+function showTestimony(index) {
+    testimonies.forEach((testimony, i) => {
+        if (i === index) {
+            testimony.classList.add('active') 
+        } else {
+            testimony.classList.remove('active') 
+        }
+    });
+}
+
+function autoSlide() {
+    showTestimony(currentIndex);
+    currentIndex = (currentIndex + 1) % testimonies.length;
+}
+
+// Iniciar el slider automáticamente cada 5 segundos (5000ms)
+setInterval(autoSlide, 3500);
+
+// Mostrar el primer testimonio al cargar la página
+showTestimony(currentIndex);
+
+
+
+
+
